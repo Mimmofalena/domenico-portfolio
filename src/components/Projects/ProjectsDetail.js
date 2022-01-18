b715d037305c25d390657c1d24780571dd3b677a
Mimmofalena <70837839+Mimmofalena@users.noreply.github.com>
2022-01-19 00:45:09 +0100

    improved styling. Added more visible title in detail pages. Reduced button size. Added :hover effect on projects images for interactivity

--- src/components/Projects/ProjectsDetail.js
+++ src/components/Projects/ProjectsDetail.js
@@ -9,93 +9,104 @@ import classes from "./Projects.module.css";
 
 const ProjectsDetail = () => {
   return (
-    <Card>
-      <h3>My Projects</h3>
-      <ul className={classes.listContainer}>
-        <li className={classes.listItem}>
-          <h4 className={classes.linkTitle}>Doctor page</h4>
-          <a href="https://mimmofalena.github.io/dottoressa-marina/">
-            <img className={classes.itemImg} src={doctorPicture} alt="Doctor" />
-          </a>
-          <p className={classes.detailDescription}>
-            This is a Doctor landing page. <br />
-            <br />I have created this project right after completing React
-            Course. I have set it up using create-react-app. In this project I
-            have used for my first time Material UI learning it from online
-            resources. I used EmailJS and Formik library to implement the
-            contact form.
-          </p>
-        </li>
-        <li className={classes.listItem}>
-          <h4 className={classes.linkTitle}>Natours</h4>
-          <a href="https://natours-domenico.herokuapp.com/">
-            <img
-              className={classes.itemImg}
-              src={natoursPicture}
-              alt="Natours"
-            />
-          </a>
-          <p className={classes.detailDescription}>
-            Natours project made with NODE JS. <br />
-            <br />I have created this project right after completing 'Node.js,
-            Express, MongoDB & More: The Complete Bootcamp 2022'. This is a tour
-            booking APP which allows you to book tours, pay the with stripe(test
-            only), and login onto your account.
-          </p>
-        </li>
-        <li className={classes.listItem}>
-          <h4 className={classes.linkTitle}>Pig game</h4>
-          <a href="https://mimmofalena.github.io/pig-game/">
-            <img
-              className={classes.itemImg}
-              src={pigGamePicture}
-              alt="Doctor"
-            />
-          </a>
-          <p className={classes.detailDescription}>
-            <strong>
-              **NOT mobile friendly, please open it on a desktop**
-            </strong>
-            <br />
-            <br /> Simple game using static HTML completed as part of a JS
-            course. It improved my overall basic JS skills.
-          </p>
-        </li>
-        <li className={classes.listItem}>
-          <h4 className={classes.linkTitle}>The Bankist</h4>
-          <a href="https://mimmofalena.github.io/The-bankist/">
-            <img
-              className={classes.itemImg}
-              src={theBankistPicture}
-              alt="Doctor"
-            />
-          </a>
-          <p className={classes.detailDescription}>
-            <strong>
-              **NOT mobile friendly, please open it on a desktop**
-            </strong>
-            <br />
-            <br /> A mockup bank webapp as part of a JS course. I have
-            implemented the concept of Modal which can be seen in the login form
-          </p>
-        </li>
-        <li className={classes.listItem}>
-          <h4 className={classes.linkTitle}>Mapty</h4>
-          <a href="https://mimmofalena.github.io/Mapty/">
-            <img className={classes.itemImg} src={maptyPicture} alt="Doctor" />
-          </a>
-          <p className={classes.detailDescription}>
-            <strong>
-              **NOT mobile friendly, please open it on a desktop**
-            </strong>
-            <br />
-            <br /> Part of a JS course. Tech used HTML,JS and CSS. This webapp
-            purpose is to add workouts which can be either running or cycling on
-            a map. I have used Leaflet as a map library.
-          </p>
-        </li>
-      </ul>
-    </Card>
+    <>
+      <h3 className={classes.detailTitle}>My Projects</h3>
+      <Card>
+        <ul className={classes.listContainer}>
+          <li className={classes.listItem}>
+            <h4 className={classes.linkTitle}>Doctor page</h4>
+            <a href="https://mimmofalena.github.io/dottoressa-marina/">
+              <img
+                className={classes.itemImg}
+                src={doctorPicture}
+                alt="Doctor"
+              />
+            </a>
+            <p className={classes.detailDescription}>
+              This is a Doctor landing page. <br />
+              <br />I have created this project right after completing React
+              Course. I have set it up using create-react-app. In this project I
+              have used for my first time Material UI learning it from online
+              resources. I used EmailJS and Formik library to implement the
+              contact form.
+            </p>
+          </li>
+          <li className={classes.listItem}>
+            <h4 className={classes.linkTitle}>Natours</h4>
+            <a href="https://natours-domenico.herokuapp.com/">
+              <img
+                className={classes.itemImg}
+                src={natoursPicture}
+                alt="Natours"
+              />
+            </a>
+            <p className={classes.detailDescription}>
+              Natours project made with NODE JS. <br />
+              <br />I have created this project right after completing 'Node.js,
+              Express, MongoDB & More: The Complete Bootcamp 2022'. This is a
+              tour booking APP which allows you to book tours, pay the with
+              stripe(test only), and login onto your account.
+            </p>
+          </li>
+          <li className={classes.listItem}>
+            <h4 className={classes.linkTitle}>Pig game</h4>
+            <a href="https://mimmofalena.github.io/pig-game/">
+              <img
+                className={classes.itemImg}
+                src={pigGamePicture}
+                alt="Doctor"
+              />
+            </a>
+            <p className={classes.detailDescription}>
+              <strong>
+                **NOT mobile friendly, please open it on a desktop**
+              </strong>
+              <br />
+              <br /> Simple game using static HTML completed as part of a JS
+              course. It improved my overall basic JS skills.
+            </p>
+          </li>
+          <li className={classes.listItem}>
+            <h4 className={classes.linkTitle}>The Bankist</h4>
+            <a href="https://mimmofalena.github.io/The-bankist/">
+              <img
+                className={classes.itemImg}
+                src={theBankistPicture}
+                alt="Doctor"
+              />
+            </a>
+            <p className={classes.detailDescription}>
+              <strong>
+                **NOT mobile friendly, please open it on a desktop**
+              </strong>
+              <br />
+              <br /> A mockup bank webapp as part of a JS course. I have
+              implemented the concept of Modal which can be seen in the login
+              form
+            </p>
+          </li>
+          <li className={classes.listItem}>
+            <h4 className={classes.linkTitle}>Mapty</h4>
+            <a href="https://mimmofalena.github.io/Mapty/">
+              <img
+                className={classes.itemImg}
+                src={maptyPicture}
+                alt="Doctor"
+              />
+            </a>
+            <p className={classes.detailDescription}>
+              <strong>
+                **NOT mobile friendly, please open it on a desktop**
+              </strong>
+              <br />
+              <br /> Part of a JS course. Tech used HTML,JS and CSS. This webapp
+              purpose is to add workouts which can be either running or cycling
+              on a map. I have used Leaflet as a map library.
+            </p>
+          </li>
+        </ul>
+      </Card>
+    </>
   );
 };
 
--- src/components/Stack/StackDetail.js
+++ src/components/Stack/StackDetail.js
@@ -18,8 +18,8 @@ import LinkButton from "../../utils/LinkButton";
 const StackDetail = () => {
   return (
     <>
+      <h3 className={classes.detailTitle}>Tech Stack</h3>
       <Card>
-        <h3 className={classes.title}>Tech Stack</h3>
         <ul className={classes.gridList}>
           <StackItem
             className={classes.javascript}
@@ -113,86 +113,89 @@ const StackDetail = () => {
             }
           />
         </ul>
+        <div className={classes.textContainer}>
+          <p>
+            I have started my journey as a self-taught front end developer in
+            November 2020.
+          </p>
+          <br />
+          The very first course I undertook with the&nbsp;
+          <a
+            style={{ textDecoration: "underline" }}
+            href="https://coursera.org/share/d19868e048232bdf5982c63fca84b95b"
+          >
+            Johns Hopkins University.
+          </a>
+          <p>
+            It covered the basics of web development (
+            <strong>HTML, CSS and JS</strong> ). Halfway through the course I
+            realised the course was outdated. I then bought another course from
+            Udemy that covered Javascript in detail(The complete Javascript
+            Course: from zero to expert by Jonas Schmedtmann). The following is
+            not an exhaustive list of all the topics covered :
+          </p>
+          <ul>
+            <br />
+            <li>
+              - Javascript fundamentals including data types and variables
+            </li>
+            <li>
+              - ES6 syntax including arrow functions, spread operator, object
+              desctructuring
+            </li>
+            <li>- How Javascript works behind the scene</li>
+            <li>- DOM manipulation</li>
+          </ul>
+          <br />
+          <p>
+            In the meantime I improved my <strong>CSS</strong> skills too. I
+            read articles, watched youtube videos and completed various
+            challenges within the 'freecodecamp' website.
+            <br /> I soon started a &nbsp;
+            <strong>React course</strong> by Maximilian Schwarzmuller. The
+            course gave a brief overview of class-based components and focused
+            mostly on functional components. I am comfortable using the most
+            important Hooks, such as useState, useEffect, useRef. I have also
+            used in some of the course projects other hooks like useContext,
+            useReducer, useMemo, useCallback. The course covered REDUX for state
+            management. I learned how to initiate component re-renders by
+            changing state or props. CSS wise, the course covered different ways
+            to style components such as with styled components and my favourite
+            which is module.css.
+            <br />
+            <br />
+          </p>
+          Right after I finished React course and after some digging, I made
+          &nbsp;
+          <a
+            style={{ textDecoration: "underline" }}
+            href="https://mimmofalena.github.io/dottoressa-marina/"
+          >
+            this project
+          </a>
+          &nbsp; using and learning <strong>Material UI</strong>.
+          <br />I have recently completed a full comprehensive&nbsp;
+          <strong>NODE</strong> js course by Jonas Schmedtmann. During this
+          course I made&nbsp;
+          <a
+            style={{ textDecoration: "underline" }}
+            href="https://natours-domenico.herokuapp.com/"
+          >
+            Natours Project
+          </a>
+          &nbsp; which involves using the following technologies :
+          <br />
+          <br />
+          <ul>
+            <li>- Node JS with Express</li>
+            <li>- Mongo DB as database</li>
+            <li>- PostMan to test API endpoints</li>
+            <li>- Deployment on Heroku</li>
+          </ul>
+          <br />I have covered many more topics during the past year of learning
+          which I am happy to discuss it further.
+        </div>
       </Card>
-      <div className={classes.textContainer}>
-        <p>
-          I have started my journey as a self-taught front end developer in
-          November 2020.
-        </p>
-        <br />
-        The very first course I undertook with the&nbsp;
-        <a
-          style={{ textDecoration: "underline" }}
-          href="https://coursera.org/share/d19868e048232bdf5982c63fca84b95b"
-        >
-          Johns Hopkins University.
-        </a>
-        <p>
-          It covered the basics of web development (
-          <strong>HTML, CSS and JS</strong> ). Halfway through the course I
-          realised the course was outdated. I then bought another course from
-          Udemy that covered Javascript in detail(The complete Javascript
-          Course: from zero to expert by Jonas Schmedtmann). The following is
-          not an exhaustive list of all the topics covered :
-        </p>
-        <ul>
-          <br />
-          <li>- Javascript fundamentals including data types and variables</li>
-          <li>
-            - ES6 syntax including arrow functions, spread operator, object
-            desctructuring
-          </li>
-          <li>- How Javascript works behind the scene</li>
-          <li>- DOM manipulation</li>
-        </ul>
-        <br />
-        <p>
-          In the meantime I improved my <strong>CSS</strong> skills too. I read
-          articles, watched youtube videos and completed various challenges
-          within the 'freecodecamp' website.
-          <br /> I soon started a &nbsp;
-          <strong>React course</strong> by Maximilian Schwarzmuller. The course
-          gave a brief overview of class-based components and focused mostly on
-          functional components. I am comfortable using the most important
-          Hooks, such as useState, useEffect, useRef. I have also used in some
-          of the course projects other hooks like useContext, useReducer,
-          useMemo, useCallback. The course covered REDUX for state management. I
-          learned how to initiate component re-renders by changing state or
-          props. CSS wise, the course covered different ways to style components
-          such as with styled components and my favourite which is module.css.
-          <br />
-          <br />
-        </p>
-        Right after I finished React course and after some digging, I made
-        &nbsp;
-        <a
-          style={{ textDecoration: "underline" }}
-          href="https://mimmofalena.github.io/dottoressa-marina/"
-        >
-          this project
-        </a>
-        &nbsp; using and learning <strong>Material UI</strong>.
-        <br />I have recently completed a full comprehensive&nbsp;
-        <strong>NODE</strong> js course by Jonas Schmedtmann. During this course
-        I made&nbsp;
-        <a
-          style={{ textDecoration: "underline" }}
-          href="https://natours-domenico.herokuapp.com/"
-        >
-          Natours Project
-        </a>
-        &nbsp; which involves using the following technologies :
-        <br />
-        <br />
-        <ul>
-          <li>- Node JS with Express</li>
-          <li>- Mongo DB as database</li>
-          <li>- PostMan to test API endpoints</li>
-          <li>- Deployment on Heroku</li>
-        </ul>
-        <br />I have covered many more topics during the past year of learning
-        which I am happy to discuss it further.
-      </div>
     </>
   );
 };
